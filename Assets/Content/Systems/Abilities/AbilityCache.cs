61b9cbbc76b22557fe6dbdae53c93f33ba0e366b
DESKTOP-NM32C13\tyler <Tyler.lazarus01@outlook.com>
2023-08-16 23:08:32 +0100

    Commit 4: - New Ability System, not made or tested as Unity crashed

---
+++ Assets/Content/Systems/Abilities/AbilityCache.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityCache : MonoBehaviour
+{
+    [SerializeField] private InputManagerBase controls;
+
+    [SerializeField] private IAbility normalAtack;
+
+    private void PerformNormalAttack()
+    {
+
+    }
+
+    private void PerformAbility1()
+    {
+
+    }
+
+    private void PerformAbility2()
+    {
+
+    }
+
+    private void PerformAbility3()
+    {
+
+    }
+}
