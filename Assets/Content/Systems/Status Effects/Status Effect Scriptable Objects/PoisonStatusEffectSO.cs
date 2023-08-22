bbe5ec5b114850870a3f28dad70aba5a64a10c9b
DESKTOP-NM32C13\tyler <Tyler.lazarus01@outlook.com>
2023-08-22 20:51:18 +0100

    Commit 13: - Serialisation Build Error fix

--- Assets/Content/Systems/Status Effects/Status Effect Scriptable Objects/PoisonStatusEffectSO.cs
+++ Assets/Content/Systems/Status Effects/Status Effect Scriptable Objects/PoisonStatusEffectSO.cs
@@ -3,26 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Poison Effect", menuName = "TSGameDev/Status Effects/New Poison Effect")]
 public class PoisonStatusEffectSO : StatusEffectSOBase
 {
-    [SerializeField] private string effectName = "Poison [Tier]";
-    [SerializeField] private bool isInstant = false;
-    [SerializeField] private int maxTicks = 10;
-    [SerializeField] private TickTime tickTime = TickTime.EveryTick;
     [Tooltip("The damage deal every tick, if the poison is set to instant it does this damange only once")]
     [SerializeField] private int damagePerTick = 1;
-
-    public override string GetEffectName() => effectName;
-    public override bool GetIsInstant() => isInstant;
-
-    public override int GetMaxTick()
-    {
-        if (isInstant)
-            return 1;
-        else
-            return maxTicks;
-    }
-
-    public override TickTime GetTickTime() => tickTime;
-
     public override void RemoveStatusEffect(IEffectable _Target)
     {
         Debug.Log("This is nothing to remove for this effect");
